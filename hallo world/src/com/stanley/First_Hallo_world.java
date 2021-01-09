a574163b828bc9ed4672658ecf720f4e17bb5b37
STANLEY LEO <stanleyitan95@gmail.com>
2021-01-09 21:59:52 +0100

    a push attempt

---
+++ hallo world/src/com/stanley/First_Hallo_world.java
@@ -0,0 +1,20 @@
+package com.stanley;
+
+public class First_Hallo_world {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			System.out.println ("Hello hows your day\n");
+		}
+		
+		//System.out.println("Hallo world\n".repeat(1000));//prints to the console 
+		
+		//else {
+			//System.out.println("Hallo Mars");
+		//}
+
+	}
+
+}
